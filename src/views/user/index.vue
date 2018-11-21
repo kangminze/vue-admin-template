b957fc055c5ccb8e9a90046ebc5590f952af81f3
康学志 <kangxuezhi@beyondcent.com>
2018-11-21 10:00:48 +0800

    修改token header传递

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,31 +1,47 @@
 <template>
 
   <div class="app-container">
-    <el-table
-      v-loading="listLoading"
-      :data="userList"
-      element-loading-text="Loading"
-      border
-      fit
-      highlight-current-row>
-      <el-table-column
-        fixed
-        prop="username"
-        label="用户名"/>
-      <el-table-column
-        fixed
-        prop="password"
-        label="密码"
-      />
-    </el-table>
+    <div class="searchClass">
+      <el-row>
+        <el-col :span="2">
+          <el-button type="primary" style="width: 100px">新增</el-button>
+        </el-col>
+        <el-col :span="22">
+          <el-input v-model="searchContent" placeholder="请输入内容" style="width: 300px">
+            <el-button slot="append" icon="el-icon-search" @click="search"/>
+          </el-input>
+        </el-col>
+      </el-row>
+    </div>
 
-    <el-pagination
-      :total="total"
-      :page-size="pageSize"
-      background
-      layout="prev, pager, next"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"/>
+    <div class="tableClass" style="padding-top: 15px">
+      <el-table
+        v-loading="listLoading"
+        :data="userList"
+        element-loading-text="Loading"
+        border
+        fit
+        highlight-current-row>
+        <el-table-column
+          fixed
+          prop="username"
+          label="用户名"/>
+        <el-table-column
+          fixed
+          prop="password"
+          label="密码"
+        />
+      </el-table>
+    </div>
+    <div class="pageClass" style="text-align: right; padding-top: 15px">
+      <el-pagination
+        :total="total"
+        :page-size="pageSize"
+        background
+        layout="prev, pager, next"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"/>
+    </div>
   </div>
 
 </template>
@@ -41,7 +57,8 @@ export default {
       listLoading: true,
       total: 0,
       currentPage: 1,
-      pageSize: 2
+      pageSize: 2,
+      searchContent: ''
     }
   },
   // 生命周期时间
@@ -52,9 +69,13 @@ export default {
   methods: {
     fetchData: function() {
       this.listLoading = true
+
       var params = {
         pageSize: this.pageSize,
         page: this.currentPage
+      }
+      if (this.searchContent !== '') {
+        params.content = this.searchContent
       }
       userListByPage(params).then(response => {
         this.userList = response.data.rows
@@ -68,6 +89,9 @@ export default {
     },
     handleCurrentChange: function(val) {
       this.currentPage = val
+      this.fetchData()
+    },
+    search: function() {
       this.fetchData()
     }
   }
